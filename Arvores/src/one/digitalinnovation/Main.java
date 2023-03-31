c3649c0d94065b52f0996c03b8747f3dfbf32fea
HermesonxN <hermesoninfo@gmail.com>
2023-03-31 10:13:25 -0300

    Arvores binarias

---
+++ Arvores/src/one/digitalinnovation/Main.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation;
+import one.digitalinnovation.models.Obj;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        ArvoreBinaria<Obj> minhArvore = new ArvoreBinaria<>();
+        minhArvore.inserir(new Obj(13));
+        minhArvore.inserir(new Obj(10));
+        minhArvore.inserir(new Obj(25));
+        minhArvore.inserir(new Obj(2));
+        minhArvore.inserir(new Obj(12));
+        minhArvore.inserir(new Obj(20));
+        minhArvore.inserir(new Obj(31));
+        minhArvore.inserir(new Obj(29));
+
+        minhArvore.exibirInOrdem();
+        minhArvore.exibirPreOrdem();
+        minhArvore.exibirPosOrdem();
+    }
+}
